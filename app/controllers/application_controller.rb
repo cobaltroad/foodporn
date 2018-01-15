7f04dc986428562d8ac28e8a8dc8e19e019757a9
Ron Dollete <ron@cobaltroad.com>
2018-01-15 09:10:55 -0500

    First pass at authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    if auth_token.blank?
+      render json: { error: 'Not authenticated' }, status: :unauthorized
+    end
+  end
+
+  def auth_token
+    @auth_token ||= if request.headers['Authorization'].present?
+                      request.headers['Authorization'].split(' ').last
+                    end
+  end
 end
