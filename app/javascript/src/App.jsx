3fa0c08ecd821ca5932a348957db7fded6076ae9
Ron Dollete <ron@cobaltroad.com>
2018-01-24 15:47:57 -0500

    Forcing main container alongside left-menu

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -26,42 +26,37 @@ class App extends React.Component {
   }
 
   render() {
+    const style = { marginLeft: '210' };
     return (
       <div>
-        <Segment>
-          <Container>
-            <Menu attached='top'>
-              <Dropdown item icon='wrench' simple>
-                <Dropdown.Menu>
-                  <Dropdown.Header>Dropdown Header 1</Dropdown.Header>
-                  <Dropdown.Item>Item</Dropdown.Item>
-                  <Dropdown.Item>
-                    <Icon name='dropdown' />
-                    <span className='text'>Nested Menu</span>
-                    <Dropdown.Menu>
-                      <Dropdown.Item>Subitem 1</Dropdown.Item>
-                      <Dropdown.Item>Subitem 2</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Menu>
-            <Segment attached='bottom'>
-              <Segment>Test</Segment>
-              <Segment>Test</Segment>
-              <Segment>Test</Segment>
-              <Segment>Test</Segment>
-              <Segment>Test</Segment>
-              <Router history={history}>
-                <div>
-                  <PrivateRoute exact path="/" component={Dashboard} />
-                  <Route path="/login" component={LoginPage} />
-                </div>
-              </Router>
-            </Segment>
-          </Container>
-          <LeftMenu />
-        </Segment>
+        <div style={style}>
+          <Menu attached='top'>
+            <Dropdown item icon='wrench' simple>
+              <Dropdown.Menu>
+                <Dropdown.Header>Dropdown Header 1</Dropdown.Header>
+                <Dropdown.Item>Item</Dropdown.Item>
+                <Dropdown.Item>
+                  <Icon name='dropdown' />
+                  <span className='text'>Nested Menu</span>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Subitem 1</Dropdown.Item>
+                    <Dropdown.Item>Subitem 2</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu>
+          <Segment attached='bottom'>
+            <Router history={history}>
+              <div>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <Route path="/login" component={LoginPage} />
+              </div>
+            </Router>
+          </Segment>
+        </div>
+
+      <LeftMenu />
       </div>
     )
   }
